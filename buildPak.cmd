77f6df623e013f440b71cd217e247fb23f3d8be3
Eugene Vlasov <eugvv@altlinux.org>
2024-03-17 23:30:46 +0500

    Первый вариант cmd для сборки Russian.pak

---
+++ buildPak.cmd
@@ -0,0 +1,10 @@
+rmdir /s /q build
+mkdir build
+mkdir build\source
+mkdir build\source\Localization
+mkdir build\source\Localization\Russian
+xcopy /s /q Gender build\source\Localization\Russian
+copy language.lsx build\source\Localization\Russian
+copy russian.xml build\source\Localization\Russian
+copy russian_to_F.xml build\source\Localization\Russian
+.\Tools\Divine.exe -g dos2de -a create-package -c lz4hc -s %cd%\build\source\ -d %cd%\build\Russian.pak
